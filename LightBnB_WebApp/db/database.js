d74e059aea0bec86f1f86046d5c75a432db7e2aa
Kyle Bautista <[kylealexanderbautista09@gmail.com]>
2025-01-21 22:18:29 -0500

    Update getUserWithEmail, getUserWithId and addUser functions into a param queries

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -16,37 +16,53 @@ const pool = new Pool({
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithEmail = function (email) {
-  let resolvedUser = null;
-  for (const userId in users) {
-    const user = users[userId];
-    if (user && user.email.toLowerCase() === email.toLowerCase()) {
-      resolvedUser = user;
-    }
-  }
-  return Promise.resolve(resolvedUser);
-};
+const getUserWithEmail = (email) => {
+
+  return pool
+    .query(`SELECT * FROM users WHERE email = $1;`, [email])
+    .then((result) => result.rows[0] || null)
+    
+    .catch((err) => {
+      console.log('Error fetching user:', err.message)
+    });
+}
 
 /**
  * Get a single user from the database given their id.
  * @param {string} id The id of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithId = function (id) {
-  return Promise.resolve(users[id]);
-};
+const getUserWithId = (id) => {
+
+  return pool
+    .query(`SELECT * FROM users WHERE id = $1;`, [id])
+    .then((result) => result.rows[0] || null)
+    
+    .catch((err) => {
+      console.log('Error fetching user by id:', err.message)
+    });
+}
 
 /**
  * Add a new user to the database.
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
-const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
-};
+const addUser = (user) => {
+  
+  return pool
+    .query(
+      `INSERT INTO users (name, email, password)
+      VALUES ($1, $2, $3)
+      RETURNING *;`,
+      [user.name, user.email, user.password]
+    )
+    .then((result) => result.rows[0])
+    
+    .catch((err) => {
+      console.log("Error adding user:", err.message)
+    });
+}
 
 /// Reservations
 
