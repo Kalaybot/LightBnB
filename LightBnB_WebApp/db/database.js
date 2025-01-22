465e1a8709d501bcb6736c87410fbcd5edc1c3a6
Kyle Bautista <[kylealexanderbautista09@gmail.com]>
2025-01-22 11:19:15 -0500

    Adjust recently change getAllReservations query to meet the requirement

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -72,11 +72,12 @@ const getAllReservations = (guest_id, limit = 10) => {
 
   return pool
     .query(
-      `SELECT reservations.id, properties.title, properties.cost_per_night, reservations.start_date, avg(rating) as average_rating
+      `SELECT properties.*, reservations.*, avg(rating) as average_rating
         FROM reservations
         JOIN properties ON reservations.property_id = properties.id
         JOIN property_reviews ON properties.id = property_reviews.property_id
         WHERE reservations.guest_id = $1
+        AND reservations.end_date >= now()::date
         GROUP BY properties.id, reservations.id
         ORDER BY reservations.start_date
         LIMIT $2;`,
